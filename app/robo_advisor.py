1b3859c207170ea65fecb9d305d24aa85efbd4d2
pcotnoir <pc2508@stern.nyu.edu>
2019-06-21 19:59:17 -0400

    create moneyformat function

--- app/robo_advisor.py
+++ app/robo_advisor.py
@@ -5,6 +5,9 @@ import json
 
 #Info Inputs
 
+def moneyformat(price):
+    return '${:,.2f}'.format(price) 
+
 url = "https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol=MSFT&apikey=demo"
 response = requests.get(url)
 #print(response.status_code) #> 200
@@ -13,6 +16,7 @@ response = requests.get(url)
 
 parsed_response = json.loads(response.text)
 last_refreshed = parsed_response["Meta Data"]["3. Last Refreshed"]
+latest_close = parsed_response["Time Series (Daily)"]["2019-02-20"]["4. close"]
 #breakpoint()
 
 #Info Outputs
@@ -21,10 +25,10 @@ print("-------------------------")
 print("SELECTED SYMBOL: XYZ")
 print("-------------------------")
 print("REQUESTING STOCK MARKET DATA...")
-print("REQUEST AT: 2018-02-20 02:00pm")
+print("REQUEST AT: 2018-02-20 02:00pm") #DO ON OWN
 print("-------------------------")
 print(f"LATEST DAY: {last_refreshed}")
-print("LATEST CLOSE: $100,000.00")
+print(f"LATEST CLOSE: {moneyformat(float(latest_close))}")
 print("RECENT HIGH: $101,000.00")
 print("RECENT LOW: $99,000.00")
 print("-------------------------")

405c1a7f2e91a4e14b837990df845ea2a2bc0947
pcotnoir <pc2508@stern.nyu.edu>
2019-06-21 11:48:11 -0400

    Set up the script and environment

---
+++ robo_advisor.py
@@ -0,0 +1,18 @@
+# app/robo_advisor.py
+
+print("-------------------------")
+print("SELECTED SYMBOL: XYZ")
+print("-------------------------")
+print("REQUESTING STOCK MARKET DATA...")
+print("REQUEST AT: 2018-02-20 02:00pm")
+print("-------------------------")
+print("LATEST DAY: 2018-02-20")
+print("LATEST CLOSE: $100,000.00")
+print("RECENT HIGH: $101,000.00")
+print("RECENT LOW: $99,000.00")
+print("-------------------------")
+print("RECOMMENDATION: BUY!")
+print("RECOMMENDATION REASON: TODO")
+print("-------------------------")
+print("HAPPY INVESTING!")
+print("-------------------------")

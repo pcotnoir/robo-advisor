44b3a196687065d47477b817343c51cbdf7888eb
pcotnoir <pc2508@stern.nyu.edu>
2019-06-21 19:45:25 -0400

    latest refresh date

--- app/robo_advisor.py
+++ app/robo_advisor.py
@@ -12,8 +12,8 @@ response = requests.get(url)
 #print(response.text)
 
 parsed_response = json.loads(response.text)
-
-breakpoint()
+last_refreshed = parsed_response["Meta Data"]["3. Last Refreshed"]
+#breakpoint()
 
 #Info Outputs
 
@@ -23,7 +23,7 @@ print("-------------------------")
 print("REQUESTING STOCK MARKET DATA...")
 print("REQUEST AT: 2018-02-20 02:00pm")
 print("-------------------------")
-print("LATEST DAY: 2018-02-20")
+print(f"LATEST DAY: {last_refreshed}")
 print("LATEST CLOSE: $100,000.00")
 print("RECENT HIGH: $101,000.00")
 print("RECENT LOW: $99,000.00")

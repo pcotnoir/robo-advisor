c203cb2d782a0ab4ad86ade5d9342e9d60087919
pcotnoir <pc2508@stern.nyu.edu>
2019-06-22 16:31:43 -0400

    clean up

--- app/robo_advisor.py
+++ app/robo_advisor.py
@@ -1,6 +1,6 @@
 # app/robo_advisor.py
 
-#imports
+#imported modules and packages
 
 import time
 import requests
@@ -15,21 +15,7 @@ load_dotenv()
 def moneyformat(price):
     return '${:,.2f}'.format(price) 
 
-    #URLs: adapted from https://stackoverflow.com/questions/23294658/asking-the-user-for-input-until-they-give-a-valid-response
-# while True:
-#     try:
-#         api_key = os.environ.get("ALPHAVANTAGE_API_KEY")
-#         stock_symbol = input("Please input the stock ticker you would like information on: ")
-#         url = f"https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol={stock_symbol}&apikey={api_key}"
-#         user_input = requests.get(url)
-#         parsed_response = json.loads(user_input.text)
-#         last_refreshed = parsed_response["Meta Data"]["3. Last Refreshed"]
-#     except KeyError:
-#         print("You entered the ticker incorrectly. Expecting a properly-formed stock symbol like 'MSFT'. Please try again: ")
-#         continue
-#     else:
-#         break
-
+#URLs: adapted from https://stackoverflow.com/questions/23294658/asking-the-user-for-input-until-they-give-a-valid-response
 
 while True:
     try:
@@ -46,32 +32,9 @@ while True:
             elif not stock_symbol.isalpha():
                 print("You entered the ticker incorrectly with a number. Expecting a properly-formed stock symbol like 'MSFT'. Please try again: ")
                 continue
+          
     else:
         break
-
-
-   # api_key = os.environ.get("ALPHAVANTAGE_API_KEY")
-   # while True:
-   #     stock_symbol = input("Please input the stock ticker you would like information on: ")
-   #     if not stock_symbol.isalpha():
-   #         print("You entered the ticker incorrectly. Please try again: ")
-   #     else:
-   #         url = f"https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol={stock_symbol}&apikey={api_key}"
-   #         user_input = requests.get(url)
-   #         #user_input = requests.get("https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol={stock_symbol}&apikey={api_key}")
-   #         if "Error" in user_input.text:
-   #             print("You entered the ticker incorrectly. Please try again: ")
-   #         else:
-   #             break
-
-#stock_symbol = "MSFT" #TO DO: accept user input
-
-#url = f"https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol={stock_symbol}&apikey={api_key}"
-#response = requests.get(url)
-
-#print(response.status_code) #> 200
-#print(type(response)) #> <class 'requests.models.Response'>
-#print(response.text)
 
 #Info Inputs
 
@@ -94,17 +57,14 @@ for chosen_date in date:
 latest_high = max(large_prices)
 latest_low = min(small_prices)
 
-#breakpoint()
-
 #Info Outputs
 
-#csv_file_path = "data/stock_prices.csv" # a relative filepath
 csv_file_path = os.path.join(os.path.dirname(__file__), "..", "data", "stock_prices.csv")
 
 csv_headers = ["timestamp", "open", "high", "low", "close", "volume"]
-with open(csv_file_path, "w") as csv_file: # "w" means "open the file for writing"
+with open(csv_file_path, "w") as csv_file: 
     writer = csv.DictWriter(csv_file, fieldnames=csv_headers)
-    writer.writeheader() # uses fieldnames set above
+    writer.writeheader() 
     
     for chosen_date in date:
         everyday_prices = time_series[chosen_date]
@@ -118,7 +78,7 @@ with open(csv_file_path, "w") as csv_file: # "w" means "open the file for writin
             })
 
 print("-------------------------")
-print("SELECTED SYMBOL:", stock_symbol)
+print(f"SELECTED SYMBOL: {stock_symbol}")
 print("-------------------------")
 print("REQUESTING STOCK MARKET DATA...")
 print("REQUEST AT: ", time.strftime("%Y-%m-%d"), time.strftime("%H:%M:%S"))

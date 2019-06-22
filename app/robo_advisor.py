07b2bf9acb9994f355a3279a925bc7ee2e4dfd5a
pcotnoir <pc2508@stern.nyu.edu>
2019-06-22 12:16:58 -0400

    sample data to .csv file

--- app/robo_advisor.py
+++ app/robo_advisor.py
@@ -1,18 +1,26 @@
 # app/robo_advisor.py
 
+#imports
+
 import requests
 import json
+import csv
+import os
 
-#Info Inputs
+#functions
 
 def moneyformat(price):
     return '${:,.2f}'.format(price) 
 
+#URLs
 url = "https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol=MSFT&apikey=demo"
 response = requests.get(url)
+
 #print(response.status_code) #> 200
 #print(type(response)) #> <class 'requests.models.Response'>
 #print(response.text)
+
+#Info Inputs
 
 parsed_response = json.loads(response.text)
 time_series = parsed_response["Time Series (Daily)"]
@@ -37,6 +45,17 @@ latest_low = min(small_prices)
 
 #Info Outputs
 
+#csv_file_path = "data/stock_prices.csv" # a relative filepath
+csv_file_path = os.path.join(os.path.dirname(__file__), "..", "data", "stock_prices.csv")
+
+with open(csv_file_path, "w") as csv_file: # "w" means "open the file for writing"
+    writer = csv.DictWriter(csv_file, fieldnames=["city", "name"])
+    writer.writeheader() # uses fieldnames set above
+    writer.writerow({"city": "New York", "name": "Yankees"})
+    writer.writerow({"city": "New York", "name": "Mets"})
+    writer.writerow({"city": "Boston", "name": "Red Sox"})
+    writer.writerow({"city": "New Haven", "name": "Ravens"})
+
 print("-------------------------")
 print("SELECTED SYMBOL: XYZ")
 print("-------------------------")
@@ -48,8 +67,10 @@ print(f"LATEST CLOSE: {moneyformat(float(latest_close))}")
 print(f"RECENT HIGH: {moneyformat(float(latest_high))}")
 print(f"RECENT LOW: {moneyformat(float(latest_low))}")
 print("-------------------------")
-print("RECOMMENDATION: BUY!")
+print("RECOMMENDATION: BUY!") #if statements required
 print("RECOMMENDATION REASON: TODO")
+print("-------------------------")
+print(f"WRITING DATA TO CSV: {csv_file_path}...")
 print("-------------------------")
 print("HAPPY INVESTING!")
 print("-------------------------")
